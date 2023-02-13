d791b83f856b47f55d5b3dc7992820711eb072e2
FliKites <72367027+FliKites@users.noreply.github.com>
2023-02-13 03:21:21 -0500

    Add support for custom port at deployment time
    This change allows you to specify what port you want to run your SSHD server on when deploying a built image, via Environment variables.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
-if [ -z "${AUTHORIZED_KEYS}" ]; then
-  echo "Need your ssh public key as AUTHORIZED_KEYS env variable. Abnormal exit ..."
+#!/bin/shif [ -z "${AUTHORIZED_KEYS}" ]; then \  echo "Need your ssh public key as AUTHORIZED_KEYS env variable. Abnormal exit ..."
   exit 1
 fi
 
 echo "Populating /root/.ssh/authorized_keys with the value from AUTHORIZED_KEYS env variable ..."
 echo "${AUTHORIZED_KEYS}" > /root/.ssh/authorized_keys
 
+    apk update \
+    && apk add openssh \
+    && mkdir /root/.ssh \
+    && chmod 0700 /root/.ssh \
+    && ssh-keygen -A \
+    && sed -i s/^#PasswordAuthentication\ yes/PasswordAuthentication\ no/ /etc/ssh/sshd_config \
+    && sed -i "s/#Port 22/Port ${PORT}/" /etc/ssh/sshd_config \
 # Execute the CMD from the Dockerfile:
 exec "$@"
-
